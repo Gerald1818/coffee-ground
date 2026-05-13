5932a4263bbb77e25ea56f76f8e8f3bb5a907bcc
Gerald1818 <pizarrojohngereald46@gmail.com>
2026-05-13 17:06:57 +0800

    fixed spacing between email and the all rights reserved

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <a href="https://www.facebook.com/CABLOCALCAFFEINEDEALER" target="_blank" rel="noreferrer">Facebook</a>
         <a href="https://www.instagram.com/coffeegroundcabuyao?igsh=MTVubW1wYjJvaHp1Mg==" target="_blank" rel="noreferrer">Instagram</a>
       </div>
-      <p>coffeegroundcabuyao@gmail.com</p>
+      <p style={{ marginBottom: '15px' }}>coffeegroundcabuyao@gmail.com</p>
       <p>©2023 Coffee Ground Cabuyao. All rights reserved.</p>
     </footer>
   );
